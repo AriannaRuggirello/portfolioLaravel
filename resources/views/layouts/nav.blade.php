ef5709d0371690ddc736e9bf450a95542c212f4d
Arianna Ruggirello <ruggirelloarianna11@gmail.com>
2024-06-06 10:55:35 +0200

    style

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'AriannaRuggirelloPortfolio') }}</title>
+
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Usando Vite -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+</head>
+
+<body>
+    <div id="app">
+
+
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
+                    <div class="logo_laravel">
+
+                    </div>
+                    <h3>AriRuggi</h3>
+                    {{-- config('app.name', 'Laravel') --}}
+                </a>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
+                        <li class="nav-item">
+                            {{-- <a class="nav-link" href="{{ url('/') }}">{{ __('Home') }}</a> --}}
+                        </li>
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#home">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#about">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#portfolio">Portfolio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#contacme">Contattami</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="">
+            @yield('content')
+        </main>
+    </div>
+</body>
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,151 @@
-@extends('layouts.app')
+@extends('layouts.nav')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8">
-                <h2>La Mia Bio</h2>
-                <p>Inserisci qui la tua biografia...</p>
+    <!-- hero -->
+    <section id="home" class="hero py-5 d-flex align-items-center">
+        <div class="m-auto" style="width: 70%;">
+
+            <div class="row align-items-center justify-content-between text-center text-lg-start py-5">
+
+                <div class="col-sm-10 col-lg-5 ">
+                    <h1 class="py-1">Junior Full Stack Web Developer &#128075;</h1>
+                    <span>Sono Arianna Ruggirello. Appassionata di codice <br> vivo in Friuli,Italia &#128205;
+                    </span>
+                    <br><br>
+                    <!-- social -->
+                    <div>
+                        <a href="https://github.com/AriannaRuggirello" target="_blank">
+                            <i class="fs-3 fa-brands fa-github me-3"></i>
+                        </a>
+                        <a href="https://www.linkedin.com/in/arianna-ruggirello-63509323a/" target="_blank">
+                            <i class="fs-3 fa-brands fa-linkedin"></i>
+                        </a>
+                    </div>
+                </div>
+                <!-- avatar icon -->
+                <div class="col-sm-10 col-lg-5 text-end">
+                    <img src="../../public/icone/avatar.jpg" style="width: 300px;" alt="">
+                </div>
+                <!-- tech skills -->
+                <div class="col-sm-10 col-lg-9">
+                    <div class="row row-cols-sm-1 row-col-lg-2 gap-1">
+
+                        <h5 class="col-lg-2 text-center text-lg-start">My skills </h5>
+                        <div class="col-lg-9">
+                            <div class="row row-cols-lg-8 justify-content-center py-2 gy-4">
+                                <img style="width: 60px;" src="../../public/icone/html-5.png" alt="">
+                                <img style="width: 60px;" src="../../public/icone/css-3.png" alt="">
+                                <img style="width: 60px;" src="../../public/icone/js.png" alt="">
+                                <img style="width: 60px;" src="../../public/icone/sass.png" alt="">
+                                <img style="width: 60px;" src="../../public/icone/bootstrap.png" alt="">
+                                <img style="width: 60px;" src="../../public/icone/Vue.js_Logo_2.svg.png" alt="">
+                                <img style="width: 60px;" src="../../public/icone/php.png" alt="">
+                                <img style="width: 60px;" src="../../public/icone/mysql.png" alt="">
+                                <img style="width: 60px;" src="../../public/icone/1969px-Laravel.svg.png" alt="">
+                            </div>
+                        </div>
+
+
+
+                    </div>
+                </div>
             </div>
-            <div class="col-md-4">
-                <h2>I Miei Progetti</h2>
-                <div class="py-12">
+        </div>
 
+    </section>
 
+    <!-- about -->
+    <section id="about">
+        <div class="m-auto" style="width: 80%;">
+            <div class="row justify-content-between align-items-center text-center text-lg-start pb-5">
+                <h4 class=" green ">CHI SONO</h4>
+                <h2 class="pb-3"><b>Il mio mondo, i miei progetti, la mia identità. Chi sono? 🧐</b></h2>
 
+                <div class="col-lg-6 py-3">
+                    <img class="rounded-circle img-thumbnail" src="../../public/myphoto.jpg" alt="">
+                </div>
+                <div class="col-sm-12 col-lg-6">
+                    <p>Ho appena concluso un corso di formazione tramite un'accademia online incentrata sullo sviluppo
+                        web.
+                        Sebbene la mia formazione attuale sia orientata verso la tecnologia ho lavorato anche nella
+                        ristorazione e nell'abbigliamento dove ho imparato come trattare con una varietà di persone e
+                        situazioni, sviluppando una buona empatia e la capacità di risolvere problemi in modo efficace.
+                    </p>
+                    <a href="../../public/cv.pdf" download="curriculum.pdf">
+                        <button type="button" class="mt-4 rounded-pill btn ">
+                            Download CV
+                        </button>
+                    </a>
 
                 </div>
 
 
+
+
             </div>
         </div>
-    </div>
+
+    </section>
+
+    <!-- contattami -->
+    <section id="contacme" class="py-5  text-center text-lg-start">
+        <div class="m-auto " style="width: 80%;">
+            <h4 class="green">CONTATTAMI</h4>
+            <h2><b>Hai domande o idee? Scrivimi! &#128071;</b></h2>
+            <div class="row pt-3 ">
+                <!-- location -->
+                <div class="col-sm-12 col-lg-4 pb-3">
+
+                    <div class="pb-2">
+                        <i class="fs-3 green fa-regular fa-map border p-2 rounded-circle"></i>
+                    </div>
+                    <div>
+                        <h5>Dove mi trovo</h5>
+                        <a href="#" class="link-offset-2 link-underline link-underline-opacity-0">Friuli,Italia</a>
+                    </div>
+
+
+                </div>
+
+                <!-- mail -->
+                <div class="col-sm-12 col-lg-4 ">
+
+                    <div class="pb-2">
+                        <i class="green fs-3 fa-regular fa-envelope border p-2 rounded-circle"></i>
+                    </div>
+                    <div>
+                        <h5>Mail</h5>
+                        <a href="mailto:ruggirelloarianna11@gmail.com?subject=Maggiori informazioni"
+                            class="link-offset-2 link-underline link-underline-opacity-0">ruggirelloarianna11@gmail.com</a>
+                    </div>
+
+                </div>
+
+
+
+            </div>
+        </div>
+
+    </section>
+
+
+    <footer class="py-5 ">
+        <div class="m-auto" style="width: 80%;">
+
+            <div class="row justify-content-lg-between ">
+                <div class="col-sm-12 col-lg-6 pb-3 text-center text-lg-start ">
+                    <h6>Copyright © 2023. All rights are reserved</h6>
+                </div>
+
+                <div class="col-sm-12 col-lg-6 text-center text-lg-end ">
+                    <a href="https://github.com/AriannaRuggirello" target="_blank">
+                        <i class="fs-3 fa-brands fa-github me-3"></i>
+                    </a>
+                    <a href="https://www.linkedin.com/in/arianna-ruggirello-63509323a/" target="_blank">
+                        <i class="fs-3 fa-brands fa-linkedin"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </footer>
 @endsection
